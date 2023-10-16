a07f9826e29304e2af244d3ded8f7018fa3bce86
Hamid <Bouayadihamid@gmail.com>
2023-10-16 13:31:24 -0700

    back to zero

--- _printf.c
+++ _printf.c
@@ -8,42 +8,25 @@
 int _printf(const char *format, ...)
 {
 	int printed = 0;
+
 	va_list args;
 
 	va_start(args, format);
-	while (*format)
+
+	while (*format != '\0')
 	{
 		if (*format != '%')
 		{
-			write(1, format, 1);
-			printed++;
+			format++;
+			printed = selector(format, args, printed);
+			format++;
 		}
 		else
 		{
+			_putchar(*format);
+			printed++;
 			format++;
-			if (*format == '\0')
-				break;
-			if (*format == '%')
-			{
-				write(1, format, 1);
-				printed++;
-			}
-			else if (*format == 'c')
-			{
-				char c = va_arg(args, int);
-
-				write(1, &c, 1);
-				printed++;
-			}
-			else if (*format == 's')
-			{
-				char *str = va_arg(args, char*);
-				int str_len = strlen(str);
-				write(1, str, str_len);
-				printed += str_len;
-			}
 		}
-		format++;
 	}
 	va_end(args);
 	return (printed);
