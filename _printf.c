a5d24ce451fb15533c7e136ee98db1b787b73c77
abdelaaziz11 <abdelkhouda055@gmail.com>
2023-10-16 09:54:38 -0700

    _printf

--- _printf.c
+++ _printf.c
@@ -1,33 +1,47 @@
 #include "main.h"
-
 /**
- * _printf - implementation of the inbuilt printf
- * @format: the format specifier
- * Return: Format string
+ * _printf - produces output according to a format
+ * @format: format specifier
+ * Return: string of format
  */
-
 int _printf(const char *format, ...)
 {
-	int printed = 0;
-
+	char c = va_arg(args, int), *str = va_arg(args, char*);
+	int printed = 0, str_len = 0;
 	va_list args;
 
 	va_start(args, format);
-
-	while (*format != '\0')
+	while (*format)
 	{
-		if (*format == '%')
+		if (*format != '%')
 		{
-			format++;
-			printed = selector(format, args, printed);
-			format++;
+			rite(1, format, 1);
+			printed++;
 		}
 		else
 		{
-			_putchar(*format);
-			printed++;
 			format++;
+			if (*format == '\0')
+				break;
+			if (*format == '%')
+			{
+				write(1, format, 1);
+				printed++;
+			}
+			else if (*format == 'c')
+			{
+				write(1, &c, 1);
+				printed++;
+			}
+			else if (*format == 's')
+			{
+			while (str[str_len] != '\0')
+				str_len++;
+			write(1, str, str_len);
+			printed += str_len;
+			}
 		}
+		format++;
 	}
 	va_end(args);
 	return (printed);
