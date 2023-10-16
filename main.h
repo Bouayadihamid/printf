f0870290cf3d665fd1e7f6eecd489cb0255d30a6
abdelaaziz11 <abdelkhouda055@gmail.com>
2023-10-16 13:26:18 -0700

    commit of main.h

--- main.h
+++ main.h
@@ -1,14 +1,22 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stdio.h>
 #include <stdarg.h>
-#include <string.h>
 #include <unistd.h>
+#include <limits.h>
 
-void print_char(va_list args);
-void print_string(va_list args);
-void print_percent(void);
 int _printf(const char *format, ...);
-int _putchar(char c);
+int _putchar(char character);
+int printf_char(va_list args, int printed);
+int printf_string(va_list args, int printed);
+int printf_integer(va_list args, int printed);
+int selector(const char *format, va_list args, int printed);
+int printf_binary(unsigned int num, int printed);
+int _x(unsigned int num, int printed, int uppercase);
+int printf_octal(unsigned int num, int printed);
+int printf_unsigned(unsigned int num, int printed);
+int printf_reverse(va_list args, int printed);
+int printf_pointer(va_list args, int printed);
 
-#endif /* MAIN_H */
+#endif
