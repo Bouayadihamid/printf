87b0550d62a68ada105c1ce9df1e19eb278d3d98
abdelaaziz11 <abdelkhouda055@gmail.com>
2023-10-17 07:38:10 -0700

    main.h

--- main.h
+++ main.h
@@ -7,12 +7,12 @@
 #include <limits.h>
 
 int _putchar(char character);
-int _printf(const char *format, ...);
 int printf_char(va_list args, int count);
 int printf_string(va_list args, int count);
 int printf_integer(va_list args, int count);
 int selector(const char *format, va_list args, int count);
 int printf_binary(unsigned int num, int count);
+int _printf(const char *format, ...);
 int printf_h(unsigned int num, int count, int uppercase);
 int printf_octal(unsigned int num, int count);
 int printf_unsigned(unsigned int num, int count);
